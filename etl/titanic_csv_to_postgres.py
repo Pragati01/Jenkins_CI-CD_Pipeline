205d3952a8fd33b176b6149b243dd34ec95957da
Pragati01 <61610945+Pragati01@users.noreply.github.com>
2025-05-06 20:18:54 +0530

    Update titanic_csv_to_postgres.py

--- etl/titanic_csv_to_postgres.py
+++ etl/titanic_csv_to_postgres.py
@@ -1,6 +1,9 @@
+import os
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
+
+
 
 # New working public CSV dataset
 url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
@@ -9,9 +12,19 @@ url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic
 df = pd.read_csv(url)
 df.columns = [col.strip().replace(" ", "_").lower() for col in df.columns]  # Clean column names
 
-conn_str = "postgresql://neondb_owner:npg_oNW2PG8Cfxgc@ep-floral-rice-a8rn0ap1-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
 
-engine = create_engine(conn_str)
+# Secure DB credentials from environment variables
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT", "5432")
+db_name = os.environ.get("DB_NAME")
+
+# SQLAlchemy engine
+engine = create_engine(f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
+
+#df.to_sql('titanic_passengers', engine, if_exists='replace', index=False)
+#engine = create_engine(conn_str)
 
 
 # Load into DB
